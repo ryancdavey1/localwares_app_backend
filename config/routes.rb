6afe99b4e04d9b82326a6442019e341f6409d1f2
ryancdavey1 <ryancdavey1@gmail.com>
2021-02-23 22:50:44 -0700

    configure routes for businesses, categories, and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :items
-  resources :businesses
+  
   resources :locations
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :categories
+      resources :items
+      resources :businesses
     end
   end
 end
